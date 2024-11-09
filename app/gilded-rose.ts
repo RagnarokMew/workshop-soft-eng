a35f3e136128029fed4a5e8ac9454bd1e51f4edc
RagnarokMew <101945097+RagnarokMew@users.noreply.github.com>
2024-11-09 20:12:46 +0200

    Refactor app code
    Signed-off-by: RagnarokMew <101945097+RagnarokMew@users.noreply.github.com>

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -8,6 +8,49 @@ export class Item {
         this.sellIn = sellIn;
         this.quality = quality;
     }
+
+    passTime() { this.sellIn -= (this.name == "Sulfuras, Hand of Ragnaros") ? 0 : 1; }
+
+    calculateQuality()  {
+        if(this.name == "Sulfuras, Hand of Ragnaros")
+          return;
+
+        let decay = 1;
+        decay = ( this.sellIn < 0 ) ? 2 : 1;
+
+        if(this.name == "Aged Brie")
+        {
+          if(this.sellIn == 0) // forced to add this because in the original code, brie quality does not share the same logic as other items
+            decay += 1;        // normally items change in quality x2 once sellIn is < 0, but for Brie it's calculated after the tick changes, thus for Brie the cond is <= 0
+
+          decay*=-1;
+        }
+
+        if(this.name == "Backstage passes to a TAFKAL80ETC concert") {
+          decay = -1;
+
+          if(this.sellIn < 11)
+            decay-=1;
+          if(this.sellIn < 6)
+            decay-=1
+
+          if(this.sellIn < 0)
+          {
+            this.quality = 0;
+            return;
+          }
+        }
+
+        this.quality -= decay
+
+        if(this.quality > 50)
+          this.quality = 50;
+
+        if(this.quality < 0)
+          this.quality = 0;
+    }
+
+
 }
 
 export class GildedRose {
@@ -19,6 +62,12 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
+
+          this.items[i].calculateQuality();
+          this.items[i].passTime();
+
+          ///old code
+      /*
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
                     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
@@ -61,7 +110,7 @@ export class GildedRose {
                         this.items[i].quality = this.items[i].quality + 1
                     }
                 }
-            }
+            }*/
         }
 
         return this.items;
